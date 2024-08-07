258b0a44de9601bd85904c401706ca4867e70997
Kavindi Hansika <kavindihansika27@gmail.com>
2024-08-07 11:37:11 +0530

    #13 frontend: employee management interfaces

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import TopNav from "./Components/TopNav/TopNav";
+import Home from "./Pages/Home/Home";
+import Employees from "./Pages/Employees/Employees";
+import Map from "./Pages/Map/Map";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <TopNav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/map" element={<Map />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </Router>
+  );
+};
 
-export default App
+export default App;
